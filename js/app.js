ec8d61a7dd97432bbdbb7fe064592083d3793dd8
Hasan-star-creat <74924051+Hasan-star-creat@users.noreply.github.com>
2021-03-25 18:13:25 +0600

    create app.js fle

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+             const clickHander1 = () => {
+             const exploring1 =document.getElementById('explor1');
+                        exploring1.style.display = 'block';
+                        }
+
+             const clickHander2 = () => {
+              const exploring2 =document.getElementById('explor2');
+                   exploring2.style.display = 'block';
+                        }
+
+             const clickHander3 = () => {
+                 const exploring3 =document.getElementById('explor3');
+                        exploring3.style.display = 'block';
+                        }
+                  
